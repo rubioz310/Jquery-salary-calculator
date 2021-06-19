93fd7383d410629cc3a86f2fd8c409be7a7add66
Jose Rubio <ru310z@gmail.com>
2021-06-19 18:36:35 -0500

    Site now saves employee information into an array

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('Salary Calculator')
+$(readyNow);
+
+function readyNow() {
+    console.log('Salary Calculator')
+    submitEmployee();
+}
+//Array for all employees
+const employeesArray = [];
+
+//Function that creates new Employee object
+function Employee(firstName, lastName, id, title, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+    this.title = title;
+    this.salary = salary;
+}
+//Function that saves all data from Employee Form to an array as an object
+function submitEmployee() {
+    $('#submitEmployeeButton').on("click", addEmployee);
+}
+//Function that listens to submit Employee button
+function addEmployee() {
+    const employee = new Employee(
+        $('#employeeFirstName').val(),
+        $('#employeeLastName').val(),
+        $('#employeeId').val(),
+        $('#employeeTitle').val(),
+        $('#employeeSalary').val()
+    );
+    employeesArray.push(employee);
+    updateEmployeesTable();
+}
+//Function that shows all employees on Employee Table
+function updateEmployeesTable() {
+    console.log(employeesArray);
+}
+//Function that deletes employee from array
